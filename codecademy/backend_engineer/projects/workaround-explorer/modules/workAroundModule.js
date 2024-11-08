50c8d4aa7d07e9dd4e468ca4aa09644acaca628a
Prabir Sarkar <prabir.sarkar@lexmark.com>
2024-11-08 16:26:53 +0530

    workAroundExplorer

---
+++ codecademy/backend_engineer/projects/workaround-explorer/modules/workAroundModule.js
@@ -0,0 +1,46 @@
+// Add your imports here.
+//This file is only concerned with digging into the data to find precise values.
+import salDataImports from './modules/salaryData.js';
+const {salaryData, getDataByRole, getDataByCompany} = salDataImports;
+// Replace the empty array with the appropriate imported function/value
+const getAverageSalaryByRole = role => {
+  const roleData = getDataByRole(role);
+  const salariesOfRole = roleData.map(obj => obj.salary);
+  return calculateAverage(salariesOfRole);
+}
+
+// Replace the empty array with the appropriate imported function/value
+const getAverageSalaryByCompany = company => {
+  const companyData = getDataByCompany(company);
+  const salariesAtCompany = companyData.map(obj => obj.salary);
+  return calculateAverage(salariesAtCompany);
+}
+
+// Replace the empty array with the appropriate imported function/value
+// Use find() when you only need the first element of an array that matches your condition in the callback function.
+const getSalaryAtCompany = (role, company) => {
+  const companyData = getDataByCompany(company);
+  const roleAtCompany = companyData.find(obj => obj.role === role);
+  return roleAtCompany.salary;
+}
+
+// Replace the empty array with the appropriate imported function/value
+const getIndustryAverageSalary = () => {
+  const allSalaries = salaryData.map(obj => obj.salary);
+  return calculateAverage(allSalaries);
+}
+
+// Helper Function. Do not edit.
+// Note: This function does not need to be exported since it is only used by the functions contained within this module.
+function calculateAverage(arrayOfNumbers) {
+  let total = 0;
+  arrayOfNumbers.forEach(number => total += number);
+  return (total / arrayOfNumbers.length).toFixed(2);
+}
+
+export {
+  getAverageSalaryByRole,
+  getSalaryAtCompany,
+  getIndustryAverageSalary,
+  getAverageSalaryByCompany,
+};
